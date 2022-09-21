fb75c2baed73a22e1f2ffc9db9fb0a56b7ea84fe
RIcardo Souza <ryszard696@yahoo.com.br>
2022-09-21 08:57:10 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ListaOrdenadaWord
+{
+    public class Program
+    {
+        public Word.Application wordApp = new Word.Application();
+        public Word.Document aDoc = null;
+
+        static void Main(string[] args)
+        {
+            var mc = new Program();
+
+            Console.WriteLine("FINALIZADO --- Arquivo Criado");
+            Console.ReadLine();
+        }
+    }
+}
